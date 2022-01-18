f0406b9ae002cc3e26bb5e088abfcb32c32ec5f9
GabrielGPena793 <kbeca.gabriel793@gmail.com>
2022-01-18 01:54:21 -0300

    Exercise

--- Estruturas_condicionais/src/com/uldemy/Exercicio05.java
+++ Estruturas_condicionais/src/com/uldemy/Exercicio05.java
@@ -1,4 +1,41 @@
 package com.uldemy;
 
+import java.util.Scanner;
+
+/*Com base na tabela abaixo, escreva um programa que leia o código de um item e a quantidade deste item. A
+        seguir, calcule e mostre o valor da conta a pagar.*/
 public class Exercicio05 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int codigoProduto, quantidade;
+        double precoTotal = 0;
+
+        System.out.println("Digite o código do produto:");
+        System.out.println("""
+                ==================================
+                Codigo |     Produto     |   Preço
+                   1   | Cachorro quente | R$ 4,00
+                   2   |    X-Salada     | R$ 4,50
+                   3   |    X-Bacon      | R$ 5,00
+                   4   | Torrada Simples | R$ 2,00
+                   5   |  Refrigerante   | R$ 1,50
+                ==================================
+                """);
+        codigoProduto = scanner.nextInt();
+
+        System.out.println("Quantos deseja:");
+        quantidade = scanner.nextInt();
+
+        switch (codigoProduto){
+            case 1 -> precoTotal = 4.00 * quantidade;
+            case 2 -> precoTotal = 4.50 * quantidade;
+            case 3 -> precoTotal = 5.00 * quantidade;
+            case 4 -> precoTotal = 2.00 * quantidade;
+            case 5 -> precoTotal = 1.50 * quantidade;
+        }
+
+        System.out.printf("Total: R$ %.2f", precoTotal);
+    }
 }
