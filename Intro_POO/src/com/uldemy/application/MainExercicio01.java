bd21476e5b7434f4baf49874490f26a6677df609
GabrielGPena793 <kbeca.gabriel793@gmail.com>
2022-01-20 02:41:19 -0300

    POO_Exercise2

--- Intro_POO/src/com/uldemy/application/MainExercicio01.java
+++ Intro_POO/src/com/uldemy/application/MainExercicio01.java
@@ -5,6 +5,13 @@ import com.uldemy.entities.Rectangle;
 import java.util.Locale;
 import java.util.Scanner;
 
+    /*
+            Fazer um programa para ler os valores da largura e altura
+            de um retângulo. Em seguida, mostrar na tela o valor de
+            sua área, perímetro e diagonal. Usar uma classe como
+            mostrado no projeto ao lado.
+      */
+
 public class MainExercicio01 {
 
     public static void main(String[] args) {
