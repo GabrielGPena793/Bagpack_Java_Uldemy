469eda028e013b8231d0a6aab699db91d7b72b9e
GabrielGPena793 <kbeca.gabriel793@gmail.com>
2022-01-27 03:44:45 -0300

    ExerciseExceptions

---
+++ Tratamento_de_excecoes/src/com/uldemy/application/program.java
@@ -0,0 +1,41 @@
+package com.uldemy.application;
+
+import com.uldemy.model.entities.Account;
+import com.uldemy.model.exceptions.WithdrawException;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class program {
+    public static void main(String[] args) {
+        try {
+            Locale.setDefault(Locale.US);
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Enter account data");
+            System.out.print("Number: ");
+            int number = scanner.nextInt();
+            scanner.nextLine();
+            System.out.print("Holder: ");
+            String holder = scanner.nextLine();
+            System.out.print("Initial balance: ");
+            Double balance = scanner.nextDouble();
+            System.out.print("Withdraw limit: ");
+            Double limit = scanner.nextDouble();
+
+            Account account = new Account(number,holder,balance,limit);
+
+            System.out.println();
+            System.out.print("Enter amount for withdraw: ");
+            Double withdraw = scanner.nextDouble();
+            account.withdraw(withdraw);
+
+        }catch (WithdrawException e){
+            System.out.println("Withdraw error: " + e.getMessage());
+        }
+
+
+
+
+    }
+}
---
+++ Tratamento_de_excecoes/src/com/uldemy/model/exceptions/WithdrawException.java
@@ -0,0 +1,8 @@
+package com.uldemy.model.exceptions;
+
+public class WithdrawException extends Exception{
+
+    public WithdrawException(String message) {
+        super(message);
+    }
+}
