505d5dd8b1af1fd9f73c9962f2ba040ae595efda
GabrielGPena793 <kbeca.gabriel793@gmail.com>
2022-02-06 19:35:21 -0300

    File e Scanner

---
+++ trabalhando_com_arquivos/src/com/uldemy/Main.java
@@ -0,0 +1,24 @@
+package com.uldemy;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        File file = new File("c:\\temp\\in.txt");
+        Scanner scanner = null;
+
+        try {
+            scanner = new Scanner(file);
+            while (scanner.hasNextLine()){
+                System.out.println(scanner.nextLine());
+            }
+        }
+        catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
